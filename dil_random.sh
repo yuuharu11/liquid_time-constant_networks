992b4643c00ad8edaa09f0c542cc191c45437985
yuuharu11 <yuuharuharuya1120@g.ecc.u-tokyo.ac.jp>
2025-07-26 09:16:27 +0000

    make replay-based_graph

--- dil_random.sh
+++ dil_random.sh
@@ -9,7 +9,7 @@ WANDB_PROJECT="ncps_continual_learning"
 declare -a results_on_own_task # 自身のタスクでの精度
 declare -a results_on_task0    # Task0での精度
 
-task_0_OUTPUT_DIR="/work/outputs/ncps/DIL/random/train_task_0"
+task_0_OUTPUT_DIR="/work/outputs/ncps/DIL/random/baseline/train_task_0"
 
 python3 train.py \
     experiment=$BASE_EXPERIMENT \
@@ -17,10 +17,9 @@ python3 train.py \
     dataset.permute=false \
     trainer.max_epochs=10 \
     hydra.run.dir=$task_0_OUTPUT_DIR \
-    train.test=true \
     wandb.project=$WANDB_PROJECT \
     wandb.name="ncps_train_0" \
-    callbacks.experiment_logger.output_file="/work/test/DIL.csv"
+    callbacks.experiment_logger.output_file="/work/test/DIL/random_baseline.csv"
 
 task_0_CHECKPOINT_PATH="${task_0_OUTPUT_DIR}/checkpoints/last.ckpt"
 
@@ -32,8 +31,8 @@ for i in {1..9}; do
   # --- 1. Task i での訓練 ---
   RUN_NAME="train_task_${i}"
   prev_task_num=$((i - 1))
-  last_checkpoint="/work/outputs/ncps/DIL/random/train_task_${prev_task_num}/checkpoints/last.ckpt"
-  OUTPUT_DIR="/work/outputs/ncps/DIL/random/train_task_${i}"
+  last_checkpoint="/work/outputs/ncps/DIL/random/baseline/train_task_${prev_task_num}/checkpoints/last.ckpt"
+  OUTPUT_DIR="/work/outputs/ncps/DIL/random/baseline/train_task_${i}"
 
   echo ""
   echo "=================================================================="
@@ -50,8 +49,7 @@ for i in {1..9}; do
     hydra.run.dir=$OUTPUT_DIR \
     wandb.project=$WANDB_PROJECT \
     wandb.name="ncps_train_${RUN_NAME}" \
-    train.test=true \
-    callbacks.experiment_logger.output_file="/work/test/DIL.csv" \
+    callbacks.experiment_logger.output_file="/work/test/DIL/random_baseline.csv" \
 
   CHECKPOINT_PATH="${OUTPUT_DIR}/checkpoints/last.ckpt"
   echo "✅ Training for Task ${i} complete. Checkpoint: ${CHECKPOINT_PATH}"
@@ -75,7 +73,7 @@ for i in {1..9}; do
           train.test_only=true \
           dataset.permute=$PERMUTE_FLAG_TEST \
           dataset.seed=$j \
-          callbacks.experiment_logger.output_file="/work/test/DIL.csv" \
+          callbacks.experiment_logger.output_file="/work/test/DIL/random_baseline.csv" \
 
   done
 done
