ed0463d5b0252e5dc61d19441c057c69933df25d
yuuharu11 <yuuharuharuya1120@g.ecc.u-tokyo.ac.jp>
2025-08-07 08:28:40 +0000

    optimize

--- train_scripts/uci_har.sh
+++ train_scripts/uci_har.sh
@@ -6,9 +6,9 @@ BASE_EXPERIMENT="ltc_ncps/uci_har"
 WANDB_PROJECT="uci_har"
 
 # --- ループさせたいハイパーパラメータのリストを定義 ---
-UNITS_LIST=(20 32 64 128 256)
-N_LAYERS_LIST=(1 2 3)
-ODE_UNFOLDS_LIST=(1 2 3)
+UNITS_LIST=(128)
+N_LAYERS_LIST=(1 2)
+ODE_UNFOLDS_LIST=(1)
 input=9
 output=6
 
@@ -20,7 +20,7 @@ for units in "${UNITS_LIST[@]}"; do
   for n_layers in "${N_LAYERS_LIST[@]}"; do
     for ode_unfolds in "${ODE_UNFOLDS_LIST[@]}"; do
       # --- 各実験で、ディレクトリ名とWandBの実行名を動的に生成 ---
-      RUN_NAME="units${units}_layers${n_layers}_unfolds${ode_unfolds}"
+      RUN_NAME="new_units${units}_layers${n_layers}_unfolds${ode_unfolds}"
       OUTPUT_DIR="outputs/ncps/uci_har/${RUN_NAME}"
 
       echo ""
@@ -34,7 +34,7 @@ for units in "${UNITS_LIST[@]}"; do
           model.n_layers=$n_layers \
           model.layer.units.1.units=$units \
           model.layer.ode_unfolds=$ode_unfolds \
-          trainer.max_epochs=10 \
+          trainer.max_epochs=100 \
           hydra.run.dir=$OUTPUT_DIR \
           wandb.project=$WANDB_PROJECT \
           wandb.name="ncps_${RUN_NAME}_UCI_HAR"
